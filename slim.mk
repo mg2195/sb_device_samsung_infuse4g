e32868804ad8e24d5a2f208f680524f33e64cadf
mg2195 <mikeyg2195@gmail.com>
2012-10-24 15:17:10 -0700

    We should now have inline kernel building

--- slim.mk
+++ slim.mk
@@ -13,22 +13,6 @@ $(call inherit-product, device/samsung/infuse4g/full_infuse4g.mk)
 # Inherit device settings
 $(call inherit-product, vendor/slim/config/common_sgs.mk)
 
-#copy kernel and modules
-PRODUCT_COPY_FILES += \
-	device/samsung/infuse4g/prebuilt/kernel/boot.img:system/slimkernel/boot.img \
-	device/samsung/infuse4g/prebuilt/kernel/boot.img:kernel \
-	device/samsung/infuse4g/prebuilt/kernel/modem.bin:modem.bin \
-        device/samsung/infuse4g/recovery.fstab:ramdisk.img \
-        device/samsung/infuse4g/recovery.fstab:recovery/root/etc/recovery.fstab \
-	device/samsung/infuse4g/prebuilt/kernel/system/lib/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
-	device/samsung/infuse4g/prebuilt/kernel/system/lib/modules/dhd.ko:system/lib/modules/dhd.ko \
-	device/samsung/infuse4g/prebuilt/kernel/system/lib/modules/vibrator.ko:system/lib/modules/vibrator.ko \
-	device/samsung/infuse4g/prebuilt/kernel/system/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-	device/samsung/infuse4g/prebuilt/kernel/utilities/busybox:utilities/busybox \
-	device/samsung/infuse4g/prebuilt/kernel/utilities/erase_image:utilities/erase_image \
-	device/samsung/infuse4g/prebuilt/kernel/utilities/flash_image:utilities/flash_image \
-	device/samsung/infuse4g/prebuilt/kernel/utilities/make_ext4fs:utilities/make_ext4fs 
-
 PRODUCT_COPY_FILES += \
 vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
@@ -37,11 +21,7 @@ ro.camera.res=8MP
 
 #copy 00check
 PRODUCT_COPY_FILES += \
-device/samsung/infuse4g/prebuilt/kernel/system/etc/init.d/00check:system/etc/init.d/00check
-
-#copy 00check
-PRODUCT_COPY_FILES += \
-device/samsung/infuse4g/prebuilt/kernel/system/etc/init.d/00check:system/etc/init.d/00check
+vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := infuse4g
